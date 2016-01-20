d2d8bc486537e00ad191fd232702b8d00c2cf517
stefan <stefan@silverdrop.trillium.lan>
2016-01-20 07:18:09 +0000

    01.1 - add circles (stacked)

--- src/javafxapplication4/JavaFXApplication4.java
+++ src/javafxapplication4/JavaFXApplication4.java
@@ -9,6 +9,7 @@ package javafxapplication4;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.effect.BoxBlur;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.StrokeType;
@@ -38,7 +39,8 @@ public class JavaFXApplication4 extends Application {
         }
         root.getChildren().add(circles);
         
-
+        circles.setEffect(new BoxBlur(10, 10, 3));
+        
         primaryStage.show();
     }
 
